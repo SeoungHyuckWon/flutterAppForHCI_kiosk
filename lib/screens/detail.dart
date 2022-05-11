121c2fb1b65c23b9e66774ad181d8a0594009828
wsh <wsh7787@naver.com>
2022-05-11 22:14:24 +0900

    won

--- lib/screens/detail.dart
+++ lib/screens/detail.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class MyDetailPage extends StatefulWidget {
-  const MyDetailPage({Key? key, required String id}) : super(key: key);
+  const MyDetailPage({Key? key, required String title}) : super(key: key);
 
   @override
   State<MyDetailPage> createState() => _MyDetailPageState();
@@ -16,14 +16,12 @@ class _MyDetailPageState extends State<MyDetailPage> {
         alignment: Alignment.center,
         child: Column(
           children: [
-            //Padding(padding: EdgeInsets.symmetric(vertical: 10, horizontal: 0)),
             Text('1. 사이즈를 선택하세요. ',
                 style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
-            Padding(padding: EdgeInsets.symmetric(vertical: 10, horizontal: 0)),
             Row(mainAxisAlignment: MainAxisAlignment.center, children: [
               SizedBox(
                 width: 140,
-                height: 160,
+                height: 120,
                 child: RaisedButton(
                     onPressed: () {
                       color:
@@ -41,7 +39,7 @@ class _MyDetailPageState extends State<MyDetailPage> {
               Padding(padding: EdgeInsets.all(10)),
               SizedBox(
                 width: 140,
-                height: 160,
+                height: 120,
                 child: RaisedButton(
                     onPressed: () {
                       print("click");
@@ -57,6 +55,44 @@ class _MyDetailPageState extends State<MyDetailPage> {
               ),
             ]),
             Text('2. 시럽을 추가하시겠습니까? ',
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
+            Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+              SizedBox(
+                width: 160,
+                height: 135,
+                child: RaisedButton(
+                    onPressed: () {
+                      color:
+                      Color.fromARGB(255, 139, 253, 131);
+                      print("click");
+                    },
+                    child: Text(
+                      "예",
+                      style:
+                          TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+                    ),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(27.5))),
+              ),
+              Padding(padding: EdgeInsets.all(10)),
+              SizedBox(
+                width: 160,
+                height: 135,
+                child: RaisedButton(
+                    onPressed: () {
+                      print("click");
+                    },
+                    color: Color.fromARGB(255, 139, 253, 131),
+                    child: Text(
+                      "아니오",
+                      style:
+                          TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+                    ),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(27.5))),
+              ),
+            ]),
+            Text('3. 얼음을 추가하시겠습니까? ',
                 style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
             Row(mainAxisAlignment: MainAxisAlignment.center, children: [
               SizedBox(
